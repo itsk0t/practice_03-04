d8d6ef8665e986c7ccc6dfb1fb65c928a1470963
itsk0t <kot3@gmail.com>
2024-04-09 02:14:19 +0500

    Работа над визуалом, исправление багов

--- practice/account/urls.py
+++ practice/account/urls.py
@@ -7,5 +7,6 @@ app_name = 'account'
 urlpatterns = [
     path('', account_view, name='account'),
     path('<int:pk>/', your_vac_detail_view, name='yor_vacancies_detail'),
+    path('<int:pk>/', your_vac_detail_view, name='yor_vacancies_detail'),
     path('<int:pk>/update', StatusUpdateView.as_view(), name='status_update'),
 ]
--- practice/applications/models.py
+++ practice/applications/models.py
@@ -1,3 +1,5 @@
+import gettext
+
 from django.contrib.auth.models import User
 from django.db import models
 
